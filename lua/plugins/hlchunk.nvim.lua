f6dd2e3a8c19a45a3599e8c2d720c28cc03fa38a
Slee Woo <mail@sleewoo.com>
2024-08-23 05:31:24 +0300

    hlchunk setup update

--- lua/plugins/hlchunk.nvim.lua
+++ lua/plugins/hlchunk.nvim.lua
@@ -4,7 +4,25 @@ return {
   config = function()
     require("hlchunk").setup({
       chunk = {
+        enable = false,
+      },
+      line_num = {
         enable = true,
+      },
+      indent = {
+        enable = true,
+        chars = {
+          "│",
+          "¦",
+          "┆",
+          "┊",
+        },
+        style = {
+          vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui"),
+        },
+      },
+      blank = {
+        enable = false,
       },
     })
   end,
