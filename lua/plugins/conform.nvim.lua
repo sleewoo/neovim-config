1c10275a71aa8988914cab64971bd3e15f405873
Slee Woo <mail@sleewoo.com>
2024-09-03 13:44:25 +0300

    conform.nvim - allow disable/enable formatting

--- lua/plugins/conform.nvim.lua
+++ lua/plugins/conform.nvim.lua
@@ -29,11 +29,13 @@ return {
         ["*"] = { "trim_whitespace" },
       },
 
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
     })
 
     vim.api.nvim_create_user_command("Format", function(args)
@@ -47,5 +49,24 @@ return {
       end
       conform.format({ async = true, lsp_fallback = true, range = range })
     end, { range = true })
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
   end,
 }
