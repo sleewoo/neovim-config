bc429edbf33ea72bdc1b8da38edc34807bbe9229
Slee Woo <mail@sleewoo.com>
2025-06-27 20:06:32 +0300

    refine: glance.nvim

--- lua/plugins/glance.nvim.lua
+++ lua/plugins/glance.nvim.lua
@@ -1,11 +1,26 @@
-vim.keymap.set("n", "gd", "<cmd>Glance definitions<cr>")
-vim.keymap.set("n", "gr", "<cmd>Glance references<cr>")
-vim.keymap.set("n", "gy", "<cmd>Glance type_definitions<cr>")
-vim.keymap.set("n", "gm", "<cmd>Glance implementations<cr>")
-
 return {
   "dnlhc/glance.nvim",
+  lazy = false,
   config = function()
-    require("glance").setup()
+    local glance = require("glance")
+    local actions = glance.actions
+
+    vim.keymap.set("n", "gd", "<cmd>Glance definitions<cr>")
+    vim.keymap.set("n", "gr", "<cmd>Glance references<cr>")
+    vim.keymap.set("n", "gy", "<cmd>Glance type_definitions<cr>")
+    vim.keymap.set("n", "gm", "<cmd>Glance implementations<cr>")
+
+    glance.setup({
+      mappings = {
+        list = {
+          ["<C-v>"] = actions.jump_vsplit, -- Open location in vertical split
+          ["<C-s>"] = actions.jump_split, -- Open location in horizontal split
+          ["<C-t>"] = actions.jump_tab, -- Open in new tab
+        },
+        preview = {
+          ["q"] = actions.close,
+        },
+      },
+    })
   end,
 }
