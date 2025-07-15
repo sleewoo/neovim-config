6129c52713a7107de6e5cf35b6c01a07cc6aad74
Slee Woo <mail@sleewoo.com>
2025-07-15 09:11:02 +0300

    replaced hlchunk.nvim with indent-blankline.nvim

---
+++ lua/plugins/indent-blankline.nvim.lua
@@ -0,0 +1,17 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  config = function()
+    require("ibl").setup({
+      indent = {
+        char = "¦",
+      },
+      scope = {
+        enabled = true,
+        show_start = false,
+        show_end = false,
+        char = "┊",
+        highlight = "LineNr",
+      },
+    })
+  end,
+}
