67500c471b01c5bb326f700f89db627891af2a8c
Slee Woo <mail@sleewoo.com>
2026-04-05 15:25:49 +0300

    update mason-lspconfig

--- lua/plugins/mason-lspconfig.nvim.lua
+++ lua/plugins/mason-lspconfig.nvim.lua
@@ -1,3 +1,22 @@
 return {
   "williamboman/mason-lspconfig.nvim",
+  config = function()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "bashls",
+        "cssls",
+        "dockerls",
+        "html",
+        "jsonls",
+        "ts_ls",
+        "mdx_analyzer",
+        "yamlls",
+        "taplo",
+        "emmet_ls",
+        "lua_ls",
+        "biome",
+      },
+      automatic_enable = true,
+    })
+  end,
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,22 +5,4 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
   },
-  config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "bashls",
-        "cssls",
-        "dockerls",
-        "html",
-        "jsonls",
-        "ts_ls",
-        "yamlls",
-        "taplo",
-        "emmet_ls",
-        "lua_ls",
-        "biome",
-      },
-      automatic_enable = true,
-    })
-  end,
 }
